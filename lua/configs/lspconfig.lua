b296bb937c09cdda51e258a7b38ab7f925273c4a
Jose Alvarado <jos.alvatorre@gmail.com>
2024-05-08 22:20:02 +0000

    add a few old lspconfig options like <space>e

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -37,3 +37,16 @@ lspconfig.efm.setup {
     }
   }
 }
+
+-- The following was partially copied from
+-- https://github.com/neovim/nvim-lspconfig?tab=readme-ov-file#suggested-configuration
+-- I had more in https://github.com/josalvatorre/personal-nvchad-custom/blob/6d8f8a1c1243b651b10159632deea611a30c11cb/configs/lspconfig.lua#L35-L74
+-- but removed them after NvChad took care of a lot more defaults.
+-- I now add them back in as needed.
+
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
