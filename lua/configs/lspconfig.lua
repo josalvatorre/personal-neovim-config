6a47f9283455351ab93422e13e3124359ba7ed97
Jose Alvarado <jos.alvatorre@gmail.com>
2024-05-10 20:57:43 +0000

    add  back in

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -40,6 +40,7 @@ lspconfig.efm.setup {
 
 -- The following was partially copied from
 -- https://github.com/neovim/nvim-lspconfig?tab=readme-ov-file#suggested-configuration
+-- which I guess got moved to https://github.com/neovim/nvim-lspconfig/blob/a3d9395455f2b2e3b50a0b0f37b8b4c23683f44a/doc/lspconfig.txt#L477-L532
 -- I had more in https://github.com/josalvatorre/personal-nvchad-custom/blob/6d8f8a1c1243b651b10159632deea611a30c11cb/configs/lspconfig.lua#L35-L74
 -- but removed them after NvChad took care of a lot more defaults.
 -- I now add them back in as needed.
@@ -50,3 +51,15 @@ vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+  end,
+})
