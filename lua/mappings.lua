2d1a2b6c46a41d54db1b5ea6ac6972e78b123421
Jose Alvarado <jos.alvatorre@gmail.com>
2024-05-29 23:04:39 +0000

    always yank to system clipboard

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,7 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- Remap 'y' to always copy to the system clipboard
+map('n', 'y', '"+y')
+map('v', 'y', '"+y')
