364237303323155fdf19967cd139dceb65ea3553
Jose Alvarado <jos.alvatorre@gmail.com>
2024-07-22 00:36:35 +0000

    Fix Codeium by trying a different event

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -91,7 +91,7 @@ return {
   },
   {
     'Exafunction/codeium.vim',
-    event = 'BufEnter',
+    event = 'VeryLazy',
     dependencies = {
       "nvim-lua/plenary.nvim",
       "hrsh7th/nvim-cmp",
