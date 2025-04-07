caf0286a3127f1294b6f33ac32d2a6949fc8bc33
Jose Alvarado <jos.alvatorre@gmail.com>
2025-04-07 22:27:25 +0000

    add recommended nvchad ui setup

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -38,6 +38,27 @@ return {
       auto_install = true,
     },
   },
+  "nvim-lua/plenary.nvim",
+  {
+    "nvim-tree/nvim-web-devicons",
+    lazy = true,
+  },
+  {
+    "nvchad/ui",
+    config = function()
+      require "nvchad"
+    end
+  },
+  {
+     "nvchad/base46",
+     lazy = true,
+     build = function()
+       require("base46").load_all_highlights()
+     end,
+  },
+  -- optional, needed for theme switcher
+  -- or just use Telescope themes
+  "nvchad/volt",
   {
     "mfussenegger/nvim-jdtls",
     ft = "java",
